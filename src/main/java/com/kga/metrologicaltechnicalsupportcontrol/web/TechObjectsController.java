d449d9d154ec54ecea6fe94f11ab0e44cff7d4eb
gakozhevnikov <geo9@yandex.ru>
2021-11-01 12:20:58 +0500

    Add @CrossOrigin to remove Access to XMLHttpRequest at 'http://localhost-8080.com_objects/' from origin 'http://localhost:4200' has been blocked by CORS policy

--- src/main/java/com/kga/metrologicaltechnicalsupportcontrol/web/TechObjectsController.java
+++ src/main/java/com/kga/metrologicaltechnicalsupportcontrol/web/TechObjectsController.java
@@ -7,6 +7,7 @@ import com.kga.metrologicaltechnicalsupportcontrol.services.impl.TechObjectServi
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/tech_objects")
 public class TechObjectsController {
 
