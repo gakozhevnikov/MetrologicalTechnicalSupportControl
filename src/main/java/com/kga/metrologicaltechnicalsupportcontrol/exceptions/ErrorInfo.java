ecee6b16aaab464931e737684b5b1df7478d9ac9
gakozhevnikov <geo9@yandex.ru>
2021-11-02 18:03:56 +0500

    Add ErrorInfo.java

--- src/main/java/com/kga/metrologicaltechnicalsupportcontrol/exceptions/ErrorInfo.java
+++ src/main/java/com/kga/metrologicaltechnicalsupportcontrol/exceptions/ErrorInfo.java
@@ -1,12 +1,14 @@
 package com.kga.metrologicaltechnicalsupportcontrol.exceptions;
 
+import java.util.List;
+
 public class ErrorInfo {
     private final String url;
     private final ErrorType type;
     private final String typeMessage;
-    private final String[] details;
+    private final List<String> details;
 
-    public ErrorInfo(CharSequence url, ErrorType type, String typeMessage, String... details) {
+    public ErrorInfo(CharSequence url, ErrorType type, String typeMessage, List<String> details) {
         this.url = url.toString();
         this.type = type;
         this.typeMessage = typeMessage;
