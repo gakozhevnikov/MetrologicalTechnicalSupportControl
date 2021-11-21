389fff9246d908dce01455f0e6e8a41061e63677
gakozhevnikov <geo9@yandex.ru>
2021-11-21 13:49:02 +0500

    Add annotation @Repository TechObjectRepository

--- src/main/java/com/kga/metrologicaltechnicalsupportcontrol/repository/interfaces/TechObjectRepository.java
+++ src/main/java/com/kga/metrologicaltechnicalsupportcontrol/repository/interfaces/TechObjectRepository.java
@@ -2,10 +2,9 @@ package com.kga.metrologicaltechnicalsupportcontrol.repository.interfaces;
 
 import com.kga.metrologicaltechnicalsupportcontrol.model.TechObject;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface TechObjectRepository extends JpaRepository<TechObject, Long> {//CrudRepository
 
     TechObject findTechObjectByTitle(String title);
