52b5c035f8909c854777bb62b01019fde9fdc512
gakozhevnikov <geo9@yandex.ru>
2021-11-21 13:49:40 +0500

    Add annotation @Repository EquipmentRepository

--- src/main/java/com/kga/metrologicaltechnicalsupportcontrol/repository/interfaces/EquipmentRepository.java
+++ src/main/java/com/kga/metrologicaltechnicalsupportcontrol/repository/interfaces/EquipmentRepository.java
@@ -2,7 +2,9 @@ package com.kga.metrologicaltechnicalsupportcontrol.repository.interfaces;
 
 import com.kga.metrologicaltechnicalsupportcontrol.model.Equipment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
     Equipment findEquipmentByTitle(String title);
 }
