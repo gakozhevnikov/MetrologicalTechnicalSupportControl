115bd2b5091e24aa28da8aa1e2357dbf1c9c521c
gakozhevnikov <geo9@yandex.ru>
2021-11-27 18:33:30 +0500

    Created and filled UserFacade

---
+++ src/main/java/com/kga/metrologicaltechnicalsupportcontrol/facade/UserFacade.java
@@ -0,0 +1,16 @@
+package com.kga.metrologicaltechnicalsupportcontrol.facade;
+
+import com.kga.metrologicaltechnicalsupportcontrol.dto.UserDTO;
+import com.kga.metrologicaltechnicalsupportcontrol.model.User;
+
+public class UserFacade {
+
+    public UserDTO userToUsetDTO(User user){
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId(userDTO.getId());
+        userDTO.setUserName(userDTO.getName());
+        userDTO.setName(user.getName());
+        userDTO.setLastname(user.getLastName());
+        return userDTO;
+    }
+}
