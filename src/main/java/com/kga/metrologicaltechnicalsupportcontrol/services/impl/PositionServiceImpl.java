05fb1f4f27819a255eaeb3295c67204429cb0cf5
gakozhevnikov <geo9@yandex.ru>
2022-01-23 19:24:27 +0500

    Complete PositionServiceImplTest and  PositionServiceImpl . All Tests passed

---
+++ src/main/java/com/kga/metrologicaltechnicalsupportcontrol/services/impl/PositionServiceImpl.java
@@ -0,0 +1,114 @@
+package com.kga.metrologicaltechnicalsupportcontrol.services.impl;
+
+
+import com.kga.metrologicaltechnicalsupportcontrol.model.Position;
+import com.kga.metrologicaltechnicalsupportcontrol.model.TechObject;
+import com.kga.metrologicaltechnicalsupportcontrol.repository.interfaces.PositionRepository;
+import com.kga.metrologicaltechnicalsupportcontrol.repository.interfaces.TechObjectRepository;
+import com.kga.metrologicaltechnicalsupportcontrol.services.interfaces.SecondTypeParameterReturnSet;
+import com.kga.metrologicaltechnicalsupportcontrol.util.WorkPlanFileToDataBase;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
+@Getter
+@Setter
+@Data
+public class PositionServiceImpl implements SecondTypeParameterReturnSet<Position, Long, TechObject> {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final WorkPlanFileToDataBase workPlanFileToDataBase;
+    private final PositionRepository positionRepository;
+    private final TechObjectRepository techObjectRepository;
+
+    @Autowired
+    public PositionServiceImpl(WorkPlanFileToDataBase workPlanFileToDataBase, PositionRepository positionRepository, TechObjectRepository techObjectRepository) {
+        this.workPlanFileToDataBase = workPlanFileToDataBase;
+        this.positionRepository = positionRepository;
+        this.techObjectRepository = techObjectRepository;
+    }
+
+    @Override
+    public Position save(Position position) {
+        log.info("Class {}, method save, value position.getTitle: {}",getClass().getName(), position.getTitle());
+        return positionRepository.save(position);
+    }
+
+    @Override
+    public List<Position> saveAllAndFlush(List<Position> positions) {
+        log.info("Class {}, method saveAllAndFlush, value position.getTitle: {}",getClass().getName(), positions);
+        return positionRepository.saveAllAndFlush(positions);
+    }
+
+    public List<Position> saveAllAndFlushFromFile() {
+        Map<String, Set<String>> mapObjectPosition =workPlanFileToDataBase.getMapObjectPosition();
+        List<Position> positionList = new ArrayList<>();
+        /*List<Object> multiFieldsList =
+                multiFieldMap.entrySet()
+                        .stream()
+                        .flatMap(e -> e.getValue()
+                                .stream()
+                                .map(o -> queryService.query(e.getKey(), queryService.property("id").eq(o))))
+                        .collect(Collectors.toList());*/
+        /*List<Position>  positionList = mapObjectPosition.entrySet()
+                .stream()
+                .flatMap(position -> position.getValue().stream()
+                        .map(valuePosition-> new Position().setTitle(valuePosition)))*/
+
+        mapObjectPosition.forEach((key, value) -> {
+            TechObject techObject = techObjectRepository.findTechObjectByTitle(key);
+            for (String title : value) {
+                Position position = new Position();
+                position.setTitle(title);
+                position.setTechObject(techObject);
+                positionList.add(position);
+            }
+        });
+        log.info("Class {}, method saveAllAndFlushFromFile, value position.getTitle: {}",getClass().getName(), positionList);
+        return positionRepository.saveAllAndFlush(positionList);
+    }
+
+    @Override
+    public void deleteAll() {
+        log.info("Class {}, method deleteAll",getClass().getName());
+        positionRepository.deleteAllInBatch();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        log.info("Class {}, method deleteById",getClass().getName());
+        positionRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Position> findAll() {
+        log.info("Class {}, method findAll",getClass().getName());
+        return positionRepository.findAll();
+    }
+
+    @Override
+    public Position getById(Long id) {
+        log.info("Class {}, method getById, value id: {}",getClass().getName(), id);
+        Optional<Position> positionOptional = positionRepository.findById(id);
+        return positionOptional.orElse(null);
+    }
+
+    @Override
+    public Position findByTitle(String title) {
+        log.info("Class {}, method findByTitle, value title {}",getClass().getName(), title);
+        return positionRepository.findPositionByTitle(title);
+    }
+
+    @Override
+    public Set<Position> findByObject(TechObject techObject) {
+        log.info("Class {}, method findByObject, value TechObject {}",getClass().getName(), techObject);
+        return positionRepository.findPositionsByTechObject(techObject);
+    }
+}
---
+++ src/test/java/com/kga/metrologicaltechnicalsupportcontrol/services/impl/PositionServiceImplTest.java
@@ -0,0 +1,208 @@
+package com.kga.metrologicaltechnicalsupportcontrol.services.impl;
+
+import com.kga.metrologicaltechnicalsupportcontrol.model.Position;
+import com.kga.metrologicaltechnicalsupportcontrol.model.TechObject;
+import com.kga.metrologicaltechnicalsupportcontrol.util.FileManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+@SpringBootTest
+public class PositionServiceImplTest {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @Value("${work-plan-file.count-tech-object}")
+    private Integer countTechObjectTest;
+
+    @Autowired
+    TechObjectServiceImpl techObjectServiceTest;
+
+    @Autowired
+    PositionServiceImpl positionService;
+
+    private List<TechObject> techObjectList;
+    String title="Title Position in test";
+    String titleSecond="Second Title Position in test";
+
+    Position positionOne;
+    Position positionTwo;
+    List<Position> positionsList;
+
+
+    @BeforeEach
+    void setUp() {
+        techObjectList = techObjectServiceTest.findAll();
+        positionOne = new Position();
+        positionOne.setTitle(title);
+        positionOne.setTechObject(techObjectList.get(0));
+        positionTwo = new Position();
+        positionTwo.setTitle(titleSecond);
+        positionTwo.setTechObject(techObjectList.get(1));
+        positionsList = new ArrayList<>();
+        positionsList.add(positionOne);
+        positionsList.add(positionTwo);
+        log.info("Class {}, method setUp, positionsList: {}", getClass().getName(), positionsList);
+        log.info("Class {}, method setUp, techObjectList: {}", getClass().getName(), techObjectList);
+        log.info("Class {}, method setUp, Objects.requireNonNull(FileManager.getWorkPlanFile()).exists(): {}" +
+                        "positionService.getWorkPlanFileToDataBase().getMapObjectPosition().isEmpty(): {}",
+                getClass().getName(), Objects.requireNonNull(FileManager.getWorkPlanFile()).exists(),
+                positionService.getWorkPlanFileToDataBase().getMapObjectPosition().isEmpty());
+        if(Objects.requireNonNull(FileManager.getWorkPlanFile()).exists() && positionService.getWorkPlanFileToDataBase().getMapObjectPosition().isEmpty()){
+            log.info("Class {}, method setUp, in statement if result true", getClass().getName());
+            positionService.getWorkPlanFileToDataBase().setMapObjectPositionFromFile();
+        }else {log.info("Class {}, method setUp, in statement if result false", getClass().getName());}
+        positionService.deleteAll();
+    }
+
+    @Test
+    void shouldMethodSaveAllAndFlushFromFileInsertPositionsInDataBase() {
+        log.info("Class {}, method shouldMethodSaveAllAndFlushFromFileInsertPositionsInDataBase, before positionService.saveAllAndFlushFromFile()", getClass().getName());
+        positionService.saveAllAndFlushFromFile();
+        log.info("Class {}, method shouldMethodSaveAllAndFlushFromFileInsertPositionsInDataBase, after positionService.saveAllAndFlushFromFile()", getClass().getName());
+        List <Position> positionList = positionService.findAll();
+        log.info("Class {}, method shouldMethodSaveAllAndFlushFromFileInsertPositionsInDataBase, positionList: {}", getClass().getName(), positionList);
+        assertThat(!positionList.isEmpty(), is(true));
+    }
+
+    @Test
+    void shouldSaveNewPositionAndFindByTitle() {
+        Position position = new Position();
+        position.setTitle(title);
+        position.setTechObject(techObjectList.get(0));
+        log.info("Class {}, method shouldSaveNewPosition, position: {}", getClass().getName(), position);
+        log.info("Class {}, method shouldSaveNewPosition, techObjectList.get(0): {}", getClass().getName(), techObjectList.get(0));
+        Position positionSave= positionService.save(position);
+        log.info("Class {}, method shouldSaveNewPosition, positionSave: {}", getClass().getName(), positionSave);
+        assertThat(positionSave.getTitle().equals(title), is(true));
+        Position positionFromDataBase = positionService.findByTitle(title);
+        log.info("Class {}, method shouldSaveNewPosition, positionFromDataBase: {}", getClass().getName(), positionFromDataBase);
+        assertThat(positionFromDataBase.getTitle().equals(title), is(true));
+    }
+
+    @Test
+    void shouldSaveAll() {
+        Position position = new Position();
+        position.setTitle(title);
+        position.setTechObject(techObjectList.get(0));
+        Position positionSecond = new Position();
+        positionSecond.setTitle(titleSecond);
+        positionSecond.setTechObject(techObjectList.get(1));
+        log.info("Class {}, method shouldSaveAll, position: {}, positionSecond: {}", getClass().getName(), position,positionSecond);
+        log.info("Class {}, method shouldSaveAll, techObjectList.get(0): {}, techObjectList.get(1): {}", getClass().getName(), techObjectList.get(0), techObjectList.get(1));
+        List<Position> positionList = new ArrayList<>();
+        positionList.add(position);
+        positionList.add(positionSecond);
+        log.info("Class {}, method shouldSaveAll, positionList: {}", getClass().getName(), positionList);
+        List<Position> positionListAfterSaveAll = positionService.saveAllAndFlush(positionList);
+        log.info("Class {}, method shouldSaveAll, positionListAfterSaveAll.size(): {}", getClass().getName(), positionListAfterSaveAll.size());
+        assertThat(positionListAfterSaveAll, hasSize(2));
+        List<String> list = new ArrayList<>();
+        for (Position positionFor : positionListAfterSaveAll) {
+            list.add(positionFor.getTitle());
+        }
+        log.info("Class {}, method shouldSaveAll, list: {}", getClass().getName(), list);
+        assertThat(list, contains(title, titleSecond));
+        List<Position> positionListFromDataBase = positionService.findAll();
+        List<String> listFromDataBase = new ArrayList<>();
+        for (Position positionFor : positionListAfterSaveAll) {
+            listFromDataBase.add(positionFor.getTitle());
+        }
+        log.info("Class {}, method shouldSaveAll, listFromDataBase: {}", getClass().getName(), listFromDataBase);
+        assertThat(listFromDataBase, contains(title, titleSecond));
+
+    }
+
+    @Test
+    void shouldDeleteAll(){
+        positionService.deleteAll();
+        assertThat(positionService.findAll().isEmpty(), is(true));
+    }
+
+    @Test
+    void shouldDeleteById(){
+        Position position = new Position();
+        position.setTitle(title);
+        position.setTechObject(techObjectList.get(0));
+        Position positionSecond = new Position();
+        positionSecond.setTitle(titleSecond);
+        positionSecond.setTechObject(techObjectList.get(1));
+        List<Position> positionList = new ArrayList<>();
+        positionList.add(position);
+        positionList.add(positionSecond);
+        List<Position> list = positionService.saveAllAndFlush(positionList);
+        Long idPositionAfterSaveAll = list.get(0).getId();
+        log.info("Class {}, method shouldDeleteById, idPositionAfterSaveAll: {}", getClass().getName(), idPositionAfterSaveAll);
+        positionService.deleteById(idPositionAfterSaveAll);
+        List<Position> positionsListArterDelete = positionService.findAll();
+        log.info("Class {}, method shouldDeleteById, idPositionAfterSaveAll: {}", getClass().getName(), positionsListArterDelete);
+        List<Long> listId = new ArrayList<>();
+        for (Position positionFor : positionsListArterDelete) {
+            listId.add(positionFor.getId());
+        }
+        assertThat(listId, not(contains(idPositionAfterSaveAll)));
+
+    }
+
+    @Test
+    void shouldGetById(){//
+        List<Position> list = positionService.saveAllAndFlush(positionsList);
+        Position positionAfterSaveAllAndFlush = list.get(0);
+        String titleAfterGetById = positionAfterSaveAllAndFlush.getTitle();//Если есть два одинаковых titla то будет ошибка т.к. возвращатся два а не один
+        Long idPositionAfterSaveAll = positionAfterSaveAllAndFlush.getId();
+        log.info("Class {}, method shouldGetById, positionAfterSaveAllAndFlush: {}", getClass().getName(), positionAfterSaveAllAndFlush);
+        Position positionAfterGetById = positionService.getById(idPositionAfterSaveAll);
+        String titleAfterGEtById = positionAfterGetById.getTitle();
+        log.info("Class {}, method shouldGetById, positionAfterGetById: {}", getClass().getName(), positionAfterGetById);
+        assertThat(titleAfterGetById.equals(titleAfterGEtById), is(true));
+    }
+
+    @Test
+    void shouldFindByTitle(){
+        log.info("Class {}, method shouldFindByTitle, positionsList: {}", getClass().getName(), positionsList);
+        List<Position> positionListMeth = positionService.saveAllAndFlush(positionsList);
+        log.info("Class {}, method shouldFindByTitle, positionList: {}", getClass().getName(), positionListMeth);
+        String titleFindByTitle = positionService.findByTitle(title).getTitle();
+        log.info("Class {}, method shouldFindByTitle, titleFindByTitle: {}", getClass().getName(), titleFindByTitle);
+        assertThat(titleFindByTitle.equals(title), is(true));
+    }
+
+    @Test
+    void shouldFindByObject(){
+        log.info("Class {}, method shouldFindByObject, positionsList: {}", getClass().getName(), positionsList);
+        List<Position> positionListMeth = positionService.saveAllAndFlush(positionsList);
+        log.info("Class {}, method shouldFindByTitle, positionList: {}", getClass().getName(), positionListMeth);
+        TechObject techObject = techObjectList.get(0);
+        log.info("Class {}, method shouldFindByTitle, techObject: {}", getClass().getName(), techObject);
+        String techObjectTitle = techObject.getTitle();
+        log.info("Class {}, method shouldFindByTitle, techObjectTitle: {}", getClass().getName(), techObjectTitle);
+        Set<Position> positionSetByObject= positionService.findByObject(techObject);
+        log.info("Class {}, method shouldFindByTitle, positionSetByObject: {}", getClass().getName(), positionSetByObject);
+        String techObjectTitleAfterGetByObject = positionSetByObject.stream().map(Position::getTechObject)
+                .collect(Collectors.toList()).get(0).getTitle();
+        log.info("Class {}, method shouldFindByTitle, techObjectTitleAfterGetByObject: {}", getClass().getName(), techObjectTitleAfterGetByObject);
+        log.info("Class {}, method shouldFindByTitle, before assertThat, techObjectTitle: {}, techObjectTitleAfterGetByObject: {}",
+                getClass().getName(), techObjectTitle, techObjectTitleAfterGetByObject);
+        assertThat(techObjectTitleAfterGetByObject.equals(techObjectTitle), is(true));
+    }
+
+    @AfterEach
+    void tearDown() {
+        log.info("Class {}, method setUp, techObjectService.deleteAll(): ", getClass().getName());
+        positionService.deleteAll();
+    }
+}
