803fe33eb4bbf1c05f826eac872dc0cda8e5d0dc
gakozhevnikov <geo9@yandex.ru>
2022-01-26 17:30:29 +0500

    Add annotation @NotEmpty for field "id"

--- src/main/java/com/kga/metrologicaltechnicalsupportcontrol/dto/AbstractBaseDTO.java
+++ src/main/java/com/kga/metrologicaltechnicalsupportcontrol/dto/AbstractBaseDTO.java
@@ -5,11 +5,14 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 @Setter
 @ToString
 public abstract class AbstractBaseDTO implements HasId {
 
+    @NotEmpty
     private Long id;
 
     public AbstractBaseDTO() {
