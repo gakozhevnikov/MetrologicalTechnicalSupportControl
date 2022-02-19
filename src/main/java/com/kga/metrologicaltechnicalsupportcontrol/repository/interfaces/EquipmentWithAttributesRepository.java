9117a2da307636540cbeac61b37759fef1465815
gakozhevnikov <geo9@yandex.ru>
2022-02-19 18:19:10 +0500

    Add method

--- src/main/java/com/kga/metrologicaltechnicalsupportcontrol/repository/interfaces/EquipmentWithAttributesRepository.java
+++ src/main/java/com/kga/metrologicaltechnicalsupportcontrol/repository/interfaces/EquipmentWithAttributesRepository.java
@@ -2,6 +2,7 @@ package com.kga.metrologicaltechnicalsupportcontrol.repository.interfaces;
 
 import com.kga.metrologicaltechnicalsupportcontrol.model.Equipment;
 import com.kga.metrologicaltechnicalsupportcontrol.model.EquipmentWithAttributes;
+import com.kga.metrologicaltechnicalsupportcontrol.model.Position;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,6 +13,8 @@ import java.util.List;
 public interface EquipmentWithAttributesRepository extends JpaRepository<EquipmentWithAttributes, Long> {
     List<EquipmentWithAttributes> findEquipmentWithAttributesBySerialNumber(String serialNumber);
     List<EquipmentWithAttributes> findEquipmentWithAttributesByEquipment_Title(String title);
+    List<EquipmentWithAttributes> findEquipmentWithAttributesByEquipment_TitleAndSerialNumber(String title, String serialNumber);
+    List<EquipmentWithAttributes> findEquipmentWithAttributesByEquipment_TitleAndSerialNumberAndPosition(String title, String serialNumber, Position position);
     List<EquipmentWithAttributes> findEquipmentWithAttributesByDateVMI(LocalDateTime dateVMI);
     List<EquipmentWithAttributes> findEquipmentWithAttributesByPosition_TitleAndPosition_TechObject_Title(String positionTitle, String techObjectTitle);
     List<EquipmentWithAttributes> findEquipmentWithAttributesByPosition_TechObject_Title(String techObjectTitle);
