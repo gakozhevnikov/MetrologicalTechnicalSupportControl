6288bb95958c663d5ce06bb1ea3d6962240d475a
gakozhevnikov <geo9@yandex.ru>
2022-02-19 18:27:55 +0500

    Add annotation

--- src/main/java/com/kga/metrologicaltechnicalsupportcontrol/repository/interfaces/TypeServiceRepository.java
+++ src/main/java/com/kga/metrologicaltechnicalsupportcontrol/repository/interfaces/TypeServiceRepository.java
@@ -3,9 +3,11 @@ package com.kga.metrologicaltechnicalsupportcontrol.repository.interfaces;
 import com.kga.metrologicaltechnicalsupportcontrol.model.maintenance.MaintenanceOperations;
 import com.kga.metrologicaltechnicalsupportcontrol.model.maintenance.TypeService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TypeServiceRepository extends JpaRepository<TypeService, Long> {
     TypeService findTypeServiceByDesignation(String designation);
     List<TypeService> findTypeServiceByMaintenanceOperations(MaintenanceOperations maintenanceOperations);
